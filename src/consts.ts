cde92ea0ec89559bf3d87fe8a9ae72594dd9bb3d
Malo Germond <github@matchalo.org>
2026-04-26 13:17:30 +0200

    :construction: Update main content

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Shaders Creative Journal';
+export const SITE_DESCRIPTION = 'My shaders learning journal: A collection of scripts, code snippets, and personal documentation as I explore and understand shaders from math to visuals, experiments to creative applications.';
