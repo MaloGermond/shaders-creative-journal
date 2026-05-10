6219e0c53fdfb1f5a79a8d20a0c43a0180b0f250
Malo Germond <github@matchalo.org>
2026-05-10 10:01:55 +0200

    :construction: Add variaous theming elements

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   optimizeDeps: {
-    include: ['codemirror','@codemirror/view','@codemirror/lang-javascript','@codemirror/commands'],
+    include: ['codemirror','@codemirror/view','@codemirror/lang-javascript','@codemirror/commands','@codemirror/autocomplete','@codemirror/language'],
   },
   build: {
     // Assure la compatibilité avec CommonJS si nécessaire
